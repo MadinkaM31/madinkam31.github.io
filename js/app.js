07758438577fe8c495c6b8d5f97b2bf8ccb5763f
MadinkaM31 <93005908+MadinkaM31@users.noreply.github.com>
2021-11-01 16:19:06 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function () {
     const header = document.querySelector('.header2');
     window.onscroll = () => {
-    	if (window.pageYOffset > 739 & window.pageYOffset <= 2150){
+    	if (window.pageYOffset > 739 & window.pageYOffset <= 2800){
     		header.classList.add('header2_fixed');
     	}
     	else{
@@ -41,3 +41,39 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+
+var index = 0;
+var slides = document.querySelectorAll(".slide");
+var dots = document.querySelectorAll(".dot");
+
+const activeSlide = n => {
+    for(slide of slides) {
+        slide.style.display = "none";
+    }
+    slides[n].style.display = "block";
+}
+
+const activeDot = n => {
+    for(dot of dots) {
+        dot.classList.remove('actived');
+    }
+    dots[n].classList.add('actived');
+}
+
+const prepareCurrentSlide = ind => {
+    activeSlide(index);
+    activeDot(index);
+}
+
+const nextSlide = () => {
+    if(index == slides.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+    }else {
+        index++;
+        prepareCurrentSlide(index);
+    }
+}
+
+setInterval(nextSlide, 3000);
